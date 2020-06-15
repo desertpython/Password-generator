c2fedf2e748a748f88a76c05f65de32952dd0119
Desert Python <mattsenneff@aol.com>
2020-06-14 21:21:00 -0700

    readMe and additions

---
+++ readMe.md
@@ -0,0 +1,5 @@
+Hello and welcome!
+
+This week I have been working on a webpage that will generate a password give select criteria. This critria includes: upper and lower case letters, numbers, as well as special characters. The characters are randomized. 
+
+<img src=/Capture1 />
--- script.js
+++ script.js
@@ -6,39 +6,47 @@ var special = ["!", "%", "^"]
 var guarenteedCharacters = []
 var availableCharacters = []
 
-
 function generatePassword(){
   var characters = 0;
   while(characters <8 || characters >128 || isNaN(characters)) {
     characters = prompt("How many characters would like; Choose a number between 8 and 128.");
 
   }
+
+function givePrompts(){
   var lower = confirm("Do you want to use lowercase characters?");
   var upper = confirm("would you like to use uppercase letters?");
   var num = confirm("You can use numbers if you like. Proceed?");
   var spec = confirm("You can also use special characters. Continue?");
 
   if (lower) {
+    console.log("lower");
     guarenteedCharacters.push(randomize(lowerCase));
     availableCharacters.concat(lowerCase);
   }
   if (upper) {
+    console.log("upper");
     guarenteedCharacters.push(randomize(upperCase));
     availableCharacters.concat(upperCase);
   }
   if (num) {
+    console.log("num");
     guarenteedCharacters.push(randomize(numbers));
     availableCharacters.concat(numbers);
   }
   if (spec) {
+    console.log("spec");
     guarenteedCharacters.push(randomize(special));
     availableCharacters.concat(special);
   }
-
   for (var i = guarenteedCharacters.length + 1; i < characters; i++){
     guarenteedCharacters.push(randomize(availableCharacters))
   } 
-  return guarenteedCharacters.join(); //google .join to make sure string is added//
+}
+
+givePrompts();
+
+  //return guarenteedCharacters.join(); //google .join to make sure string is added//
 }
 
 function randomize(characterArray) {
